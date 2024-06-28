ffd4b83db4f2668f49c9df8209ebcf6a53a4b9d3
Eugene Menski <eugene.menski@gmail.com>
2024-06-28 10:45:20 +0400

    Reworked page components

--- admin/dashboard/src/App.js
+++ admin/dashboard/src/App.js
@@ -1,45 +1,120 @@
-import './App.css';
-import Header from "./components/Header";
-import WordForm from "./components/WordForm";
-import WordCard from "./components/Card";
+function Header() {
+  return (
+      <div id="header" className="row my-5">
+          <h1 className="text-center">Tresaurus Admin Application</h1>
+      </div>
+  );
+}
 
-function App() {
-    let words = [
-        {wordName: "Fruit", partOfSpeech: "noun", definition: "A short definition"},
-        { wordName: "Milk", partOfSpeech: "noun", definition: "A short definition"},
-        { wordName: "Change", partOfSpeech: "verb", definition: "A short definition"},
-        { wordName: "Sentence", partOfSpeech: "noun", definition: "A short definition"},
-        { wordName: "Abbreviation", partOfSpeech: "noun", definition: "A short definition"}
-  ]
-
+function WordFormHeader() {
     return (
-    <div className="App">
-        <div className="container">
-            <Header/>
-            <div className="row">
-
-                <div className="col-md-5">
-                    <h2 className="py-3">Add New Word</h2>
-                    <WordForm/>
-                </div>
-
-                <div className="col-md-7">
-                    <h2 className="py-3">List of Words</h2>
-                    <ul>
-                        {words.map(
-                            (el) => {
-                                return (
-                                    <WordCard key={el.id} word={el}/>
-                                )
-                            }
-                        )}
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-    </div>
+        <h4 className="mb-3">Add a New Word</h4>
     );
 }
 
-export default App;
+function WordForm() {
+    return (
+        <form method="post" action="/add_word">
+          <div className="mb-3">
+              <label htmlFor="name" className="form-label">Word:</label>
+              <input type="text" className="form-control" name="name" id="name" required/>
+          </div>
+          <div className="mb-3">
+              <label htmlFor="part_of_speech" className="form-label">Part of Speech:</label>
+              <input type="text" className="form-control" name="part_of_speech" id="part_of_speech"
+                     required/>
+          </div>
+          <div className="mb-3">
+              <label htmlFor="definition" className="form-label">Definition:</label>
+              <input type="text" className="form-control" name="definition" id="definition" required/>
+          </div>
+          <button type="submit" className="btn btn-primary">Save</button>
+      </form>
+  );
+}
+
+function AddNewWord() {
+    return (
+        <div>
+            <WordFormHeader/>
+            <WordForm/>
+        </div>
+    );
+}
+
+function WordListHeader() {
+    return (
+        <h4 className="mb-3">List of Words</h4>
+    );
+}
+
+function WordCard({word}) {
+    return (
+        <div className="border-1 shadow-sm rounded-3 p-3 mb-4">
+            <p className="h5 text-center mb-3"><span>{word.name}</span></p>
+            <div><span>({word.partOfSpeech}) {word.definition}</span></div>
+        </div>);
+}
+
+function WordList({words}) {
+    const cards = [];
+
+    words.forEach((word) => {
+        cards.push(
+            <WordCard
+                word={word}
+                key={word.name}/>
+        );
+    });
+
+    return (
+        <div>{cards}</div>
+    );
+}
+
+function SearchBar() {
+    return (
+        <form>
+            <div className="mb-3">
+                <input type="text" className="form-control" placeholder="Search..."/>
+            </div>
+        </form>
+    );
+}
+
+function FilterableWordList({words}) {
+    return (
+        <div>
+            <WordListHeader/>
+            <SearchBar/>
+            <WordList words={words} />
+    </div>
+  );
+}
+
+const WORDS = [
+  {name: "Apple", partOfSpeech: "noun", definition: "A short definition"},
+  {name: "Dragonfruit", partOfSpeech: "verb", definition: "A short definition"},
+  {name: "Passionfruit", partOfSpeech: "noun", definition: "A short definition"},
+  {name: "Spinach", partOfSpeech: "verb", definition: "A short definition"},
+  {name: "Pumpkin", partOfSpeech: "noun", definition: "A short definition"},
+  {name: "Peas", partOfSpeech: "noun", definition: "A short definition"}
+];
+
+export default function App() {
+  return (
+      <div className="container">
+          <div className="row">
+              <Header/>
+          </div>
+          <div className="row">
+              <div className="col-md-5">
+                  <AddNewWord/>
+              </div>
+              <div className="col-md-7">
+                  <FilterableWordList words={WORDS}/>
+              </div>
+          </div>
+      </div>
+  );
+}
