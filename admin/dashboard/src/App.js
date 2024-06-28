7c222122a29b493f49b5971db3f1b2899f5ee161
Eugene Menski <eugene.menski@gmail.com>
2024-06-28 12:36:51 +0400

    Added search to the list of words

--- admin/dashboard/src/App.js
+++ admin/dashboard/src/App.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 function Header() {
   return (
       <div id="header" className="row my-5">
@@ -8,28 +10,38 @@ function Header() {
 
 function WordFormHeader() {
     return (
-        <h4 className="mb-3">Add a New Word</h4>
+        <div className="mb-3">
+            <h4>Add a New Word</h4>
+        </div>
     );
 }
 
 function WordForm() {
     return (
         <form method="post" action="/add_word">
-          <div className="mb-3">
-              <label htmlFor="name" className="form-label">Word:</label>
-              <input type="text" className="form-control" name="name" id="name" required/>
-          </div>
-          <div className="mb-3">
-              <label htmlFor="part_of_speech" className="form-label">Part of Speech:</label>
-              <input type="text" className="form-control" name="part_of_speech" id="part_of_speech"
-                     required/>
-          </div>
-          <div className="mb-3">
-              <label htmlFor="definition" className="form-label">Definition:</label>
-              <input type="text" className="form-control" name="definition" id="definition" required/>
-          </div>
-          <button type="submit" className="btn btn-primary">Save</button>
-      </form>
+            <div className="mb-3">
+                <label for="name" className="form-label">Word</label>
+                <input type="text" className="form-control" name="name" id="name" required/>
+            </div>
+            <div className="mb-3">
+                <label for="partOfSpeech" className="form-label">Part of Speech</label>
+                <select className="form-select" name="partOfSpeech" id="partOfSpeech" aria-label="Parts of speech">
+                    <option selected>Noun</option>
+                    <option value="1">Pronoun</option>
+                    <option value="2">Verb</option>
+                    <option value="3">Adjective</option>
+                    <option value="4">Adverb</option>
+                    <option value="5">Preposition</option>
+                    <option value="6">Conjunction</option>
+                    <option value="7">Interjection</option>
+                </select>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="definition" className="form-label">Definition</label>
+                <textarea className="form-control" rows="3" name="definition" id="definition" required></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Add Word</button>
+        </form>
   );
 }
 
@@ -44,7 +56,9 @@ function AddNewWord() {
 
 function WordListHeader() {
     return (
-        <h4 className="mb-3">List of Words</h4>
+        <div className="mb-3">
+            <h4>List of Words</h4>
+        </div>
     );
 }
 
@@ -56,10 +70,11 @@ function WordCard({word}) {
         </div>);
 }
 
-function WordList({words}) {
+function WordList({words, filterText}) {
     const cards = [];
 
     words.forEach((word) => {
+        if (word.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {return;}
         cards.push(
             <WordCard
                 word={word}
@@ -72,22 +87,30 @@ function WordList({words}) {
     );
 }
 
-function SearchBar() {
+function SearchBar({filterText, onFilterTextChange}) {
     return (
         <form>
             <div className="mb-3">
-                <input type="text" className="form-control" placeholder="Search..."/>
+                <input type="text"
+                       value={filterText}
+                       onChange={(e) => onFilterTextChange(e.target.value)}
+                       className="form-control"
+                       placeholder="Search..."/>
             </div>
         </form>
     );
 }
 
 function FilterableWordList({words}) {
+    const [filterText, setFilterText] = useState('');
     return (
         <div>
             <WordListHeader/>
-            <SearchBar/>
-            <WordList words={words} />
+            <SearchBar
+                filterText={filterText}
+                onFilterTextChange={setFilterText} />
+            <WordList words={words}
+                      filterText={filterText} />
     </div>
   );
 }
