bdc939cb9ce0cec96d195027ba56e174d36e493e
Eugene Menski <eugene.menski@gmail.com>
2024-07-01 16:59:48 +0400

    Admin: added navigation

--- admin/dashboard/src/components/AppFooter.js
+++ admin/dashboard/src/components/AppFooter.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="text-center">&copy; {(new Date().getFullYear())} Tresaurus</div>;
+  return <div className="text-center">&copy; {(new Date().getFullYear())} Tresaurus (admin application)</div>;
 };
 
 export default Footer;
