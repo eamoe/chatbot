9183a548c6b8f8459a2b2cf598bdf389bff36037
Eugene Menski <eugene.menski@gmail.com>
2024-07-01 19:03:49 +0400

    Admin: make the card clickable

--- admin/dashboard/src/components/word-list/Card.js
+++ admin/dashboard/src/components/word-list/Card.js
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
 const WordCard = ({ word }) => {
-    let {wordName, partOfSpeech, definition} = word
-        return (
-        <div className="border-1 shadow-sm rounded-3 p-3 mb-4">
-            <p className="h5 text-center mb-3"><span>{wordName.charAt(0).toLowerCase() + wordName.slice(1)}</span></p>
-            <div><span>({partOfSpeech}) {definition}</span></div>
-        </div>);
-    }
+
+    let {_id, wordName, partOfSpeech, definition} = word
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/words/${_id}`);
+    };
+
+    return (
+        <div onClick={handleClick} style={{cursor: "pointer"}}>
+            <div className="border-1 shadow-sm rounded-3 p-3 mb-4">
+                <p className="h5 text-center mb-3">
+                    <span>{wordName.charAt(0).toLowerCase() + wordName.slice(1)}</span>
+                </p>
+                <div><span>({partOfSpeech}) {definition}</span></div>
+            </div>
+        </div>
+    );
+}
 
 export default WordCard
