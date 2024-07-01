a639e1dcc720cbdeb64c624d788dbda275096e6e
Eugene Menski <eugene.menski@gmail.com>
2024-07-01 11:35:13 +0400

    Extended api origins

--- api/main.py
+++ api/main.py
@@ -11,6 +11,8 @@ origins = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://localhost:8000",
+    "http://10.211.55.2",
+    "http://10.211.55.2:8000",
 ]
 
 app = FastAPI()
