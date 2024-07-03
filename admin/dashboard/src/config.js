b0ce273278d9eceaa7499fdc313f56448803b5d3
Eugene Menski <eugene.menski@gmail.com>
2024-07-03 11:45:34 +0400

    Fix

--- admin/dashboard/src/config.js
+++ admin/dashboard/src/config.js
@@ -1,3 +1,3 @@
 export const adminConfig = {
-    REACT_APP_BASE_URL: process.env.REACT_APP_BASE_URL || 'http://localhost:8000'
+    REACT_APP_BASE_URL: process.env.REACT_APP_BASE_URL || 'http://10.211.55.11:8475'
 }
